e06aef64556ed3be5a94058b5ab049121ebb451b
cody <cjonesfatloaf@gmail.com>
2024-05-02 23:20:27 -0400

    enabled some blocks and made the character limit bigger for status2d

--- config.h
+++ config.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"sb-tasks",	10,	26},
-	/* {"",	"sb-music",	0,	11}, */
+	{"",	"sb-music",	0,	11},
 	{"",	"sb-pacpackages",	0,	8},
 	{"",	"sb-news",		0,	6},
 	/* {"",	"sb-price xmr-btc \"Monero to Bitcoin\" 🔒 25",	9000,	25}, */
@@ -13,24 +13,26 @@ static const Block blocks[] = {
 	/* {"",	"sb-price btc Bitcoin 💰 21",			9000,	21}, */
 	{"",	"sb-torrent",	20,	7},
 	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
+	/* {"",	"sb-gpu",	5,	28}, */
+	/* {"",	"sb-cpu",		5,	18}, */
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	{"",	"sb-doppler",	0,	13},
 	{"",	"sb-forecast",	18000,	5},
 	{"",	"sb-mailbox",	180,	12},
-	{"",	"sb-nettraf",	1,	16},
+	/* {"",	"sb-nettraf",	1,	16}, */
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
 	/* {"",	"sb-iplocate", 0,	27}, */
-	{"",	"sb-help-icon",	0,	15},
+	/* {"",	"sb-help-icon",	0,	15}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = " | ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
 
 // autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+
--- dwmblocks.c
+++ dwmblocks.c
@@ -9,7 +9,7 @@
 #include <sys/signalfd.h>
 #include <poll.h>
 #define LENGTH(X) (sizeof(X) / sizeof (X[0]))
-#define CMDLENGTH		50
+#define CMDLENGTH		1024
 
 typedef struct {
 	char* icon;
